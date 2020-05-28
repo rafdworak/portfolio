2282524e9d0464a125cc94c738abcfd3056ef006
rafdworak <rafdworak@gmail.com>
2020-05-28 19:20:06 +0100

    header added (not-sticky)

--- src/App.js
+++ src/App.js
@@ -2,37 +2,49 @@ import React from 'react';
 import './App.css';
 import ReactFullpage from '@fullpage/react-fullpage';
 
+const anchors = ['Portfolio', 'About-me', 'Contact', 'Other-projects'];
 
 
 function App() {
   return (
+
     <ReactFullpage
-    //fullpage options
-    licenseKey = {'YOUR_KEY_HERE'}
-    scrollingSpeed = {1000} /* Options here */
-    
-    render={({ state, fullpageApi }) => {
-      return (
-        <ReactFullpage.Wrapper>
-          <div className="section" id='section-portfolio'>
-            <p>Portfolio:</p>
-            <button onClick={() => fullpageApi.moveSectionDown()}>
-              Click me to move down
-            </button>
+      //fullpage options
+      licenseKey={'YOUR_KEY_HERE'}
+      scrollingSpeed={500} /* Options here */
+      anchors={anchors}
+      navigation
+      navigationTooltips={anchors}
+      
+      render={({ state, fullpageApi }) => {
+        return (
+          <div className='portfolio'>
+            <div className='header'>
+              <div className="container">
+                <div className='link'><a href="#section-portfolio">Portfolio</a></div>
+                <div className='link'><a href="#section-portfolio">About me</a></div>
+                <div className='link'><a href="#section-contact">Contact</a></div>
+                <div className='link'><a href="#section-other">Other projects</a></div>
+              </div>
+            </div>
+            <ReactFullpage.Wrapper>
+              <div className="section" id='section-portfolio'>
+                <p>Portfolio:</p>
+              </div>
+              <div className="section" id='section-about'>
+                <p>About me</p>
+              </div>
+              <div className="section" id='section-contact'>
+                <p>Contact</p>
+              </div>
+              <div className="section" id='section-other'>
+                <p>Other projects</p>
+              </div>
+            </ReactFullpage.Wrapper>
           </div>
-          <div className="section"id='section-about'>
-              <p>About me</p>
-          </div>
-          <div className="section" id='section-contact'>
-            <p>Contact</p>
-          </div>
-          <div className="section" id='section-other'>
-            <p>Other projects</p>
-          </div>
-        </ReactFullpage.Wrapper>
-      );
-    }}
-  />
+        );
+      }}
+    />
   );
 }
 
